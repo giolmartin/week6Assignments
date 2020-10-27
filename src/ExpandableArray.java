1152e7fb1b5e2d4475ce9bc59e56cb30775745d1
Gio <giovanni.lmartin@gmail.com>
2020-10-26 23:46:18 -0500

    FinalCommit

--- src/ExpandableArray.java
+++ src/ExpandableArray.java
@@ -1,4 +1,45 @@
+import java.util.ArrayList;
+
+
 
 public class ExpandableArray {
 
+	private ArrayList<Object> list = new ArrayList<Object>();
+
+	public ExpandableArray() {
+
+	}
+
+	public void set(int index, Object value) {
+
+		ArrayList<Object> newList = new ArrayList<Object>();
+
+		if (index > 0) {
+			if (list.size() != 0) {
+				newList = list;
+			} 
+			if (list.size() > index) {
+				newList.remove(index);
+				newList.add(index, value);
+			} else if(list.size() < index) {
+				for(int i = list.size(); i < index; i++) {
+					newList.add(null);
+				} 
+				newList.add(value);
+				list = newList;	
+			}
+		}
+	}
+
+	public Object get (int index) {
+		Object obj;
+		try {
+
+			obj = list.get(index);
+		} catch(IndexOutOfBoundsException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return obj;
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,24 @@ public class Main extends ConsoleProgram {
 
 	public void run() {
 		
+		ExpandableArray array = new ExpandableArray();
+		
+		array.set(5, "Bye");
+		array.set(3, "Hello");
+		array.set(30, "Growing");
+		array.set(19, "is");
+		array.set(10, "List");
+		
+			
+		System.out.println(array.get(3));
+		System.out.println(array.get(10));
+		System.out.println(array.get(19));
+		System.out.println(array.get(30));
+		System.out.println(array.get(5));
+		
+		
+		
+		
+		
 	}
 }
